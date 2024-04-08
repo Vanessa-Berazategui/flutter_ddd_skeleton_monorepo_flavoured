6862e1cd0acee1136ef8a5843d3131b163a3589f
Vanessa Berazategui <vberazategui@geocom.com.uy>
2024-04-08 12:20:54 -0300

    chore: update material theme usage

--- apps/flutter_ddd_skeleton_monorepo_flavour_1/lib/app/view/app.dart
+++ apps/flutter_ddd_skeleton_monorepo_flavour_1/lib/app/view/app.dart
@@ -2,6 +2,7 @@
 
 // Project imports:
 import 'package:flutter_ddd_skeleton_monorepo_flavour_1/app/app.dart';
+import 'package:flutter_ddd_skeleton_monorepo_flavour_1/app/theme/material_theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -10,8 +11,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: appRouter,
-      theme: theme,
-      darkTheme: darkTheme,
+      theme: getIt<MaterialTheme>().light(),
+      darkTheme: getIt<MaterialTheme>().dark(),
       themeMode: ThemeMode.light,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
